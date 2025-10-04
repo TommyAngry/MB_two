3e32e0d01cc802559f2da07b5db146f3ce45377a
TommyAngry <surinov.36@gmail.com>
2025-10-04 17:42:38 +0400

    Learn 6

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import  Post
+from .models import  Post, Comments
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "author")
+    ''' лист дисплей чтобы данные по заголовку и автору красиво отображались'''
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ("name", "post")
     ''' лист дисплей чтобы данные по заголовку и автору красиво отображались'''
 
 # Register your models here.
